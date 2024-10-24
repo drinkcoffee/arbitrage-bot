7c0753f253d46c3d2ecb9e6a42a39da1ce3a654d
Peter Robinson <drinkcoffee@eml.cc>
2024-10-24 16:14:53 +1000

    Break code but try to move forward

--- pools/src/main.rs
+++ pools/src/main.rs
@@ -1,11 +1,14 @@
+mod pool;
+
 use alloy::providers::ProviderBuilder;
 use eyre::Result;
 use uniswap_sdk_core::{prelude::*, token};
 use uniswap_v3_sdk::prelude::*;
 
+use pool;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Construct provider
     let rpc_url = "https://rpc.immutable.com".parse()?;
     let provider = ProviderBuilder::new().on_http(rpc_url);
 
@@ -13,22 +16,49 @@ async fn main() -> Result<()> {
     let factory_address = address!("56c2162254b0E4417288786eE402c2B41d4e181e");
     let eth = token!(1, "52A6c53869Ce09a731CD772f245b97A4401d3348", 18);
     let imx = token!(1, "3A0C2Ba54D6CBd3121F01b96dFd20e99D1696C9D", 18);
-    let pool = Pool::from_pool_key(
-        13371,
-        factory_address,
-        eth.address,
-        imx.address,
-        FeeAmount::MEDIUM,
-        provider,
-        None,
-    )
-    .await?;
 
-    // Use the pool
-    println!("token0:    {:?}", pool.token0.symbol().unwrap());
-    println!("token1:    {:?}", pool.token1.symbol().unwrap());
-    println!("pool addr: {:?}", pool.address(None, Some(factory_address)));
-    println!("liquidity: {:?}", pool.liquidity);
+    // TODO loop around in a four loop for the various standard fee levels.
+    // TODO get this to work
+    med_pool = APoolType{
+        provider: provider,
+        chain_id: 13371,
+        factory_address: factory_address,
+        tok0: eth,
+        tok1: imx,
+        fee: FeeAmount::MEDIUM,
+    };
+
+
+
+
+
+// Old code.
+
+
+    // // Construct provider
+    // let rpc_url = "https://rpc.immutable.com".parse()?;
+    // let provider = ProviderBuilder::new().on_http(rpc_url);
+
+    // // Construct a pool
+    // let factory_address = address!("56c2162254b0E4417288786eE402c2B41d4e181e");
+    // let eth = token!(1, "52A6c53869Ce09a731CD772f245b97A4401d3348", 18);
+    // let imx = token!(1, "3A0C2Ba54D6CBd3121F01b96dFd20e99D1696C9D", 18);
+    // let pool = Pool::from_pool_key(
+    //     13371,
+    //     factory_address,
+    //     eth.address,
+    //     imx.address,
+    //     FeeAmount::MEDIUM,
+    //     provider,
+    //     None,
+    // )
+    // .await?;
+
+    // // Use the pool
+    // println!("token0:    {:?}", pool.token0.symbol().unwrap());
+    // println!("token1:    {:?}", pool.token1.symbol().unwrap());
+    // println!("pool addr: {:?}", pool.address(None, Some(factory_address)));
+    // println!("liquidity: {:?}", pool.liquidity);
 
     Ok(())
 }
---
+++ pools/src/pool.rs
@@ -0,0 +1,45 @@
+use alloy::providers::{ProviderBuilder, RootProvider};
+use eyre::Result;
+use uniswap_sdk_core::{prelude::*, token};
+use uniswap_v3_sdk::prelude::*;
+
+
+struct PoolType {
+    // TODO import Http and Client
+    provider: RootProvider<Http<Client>>,
+    chain_id: ChainId,
+    factory_address: Address,
+    tok0: Token,
+    tok1: Token,
+    fee: FeeAmount,
+}
+
+
+impl APool for PoolType {
+    async fn check_pool(&self) -> Result<()> {
+        // Construct a pool
+        let pool = Pool::from_pool_key(
+            self.chain_id,
+            self.factory_address,
+            self.tok0.address,
+            self.tok1.address,
+            self.fee,
+            self.provider,
+            None,
+        )
+        .await?;
+
+        // Use the pool
+        println!("Liquidity Pool: ");
+        // TODO need to catch the following call if the contract doesn't exist
+        println!(" Token0:    {:?}", pool.token0.symbol().unwrap());
+        println!(" Token1:    {:?}", pool.token1.symbol().unwrap());
+        println!(" Fee tier:  {:?}", self.fee);
+        println!(" Pool addr: {:?}", pool.address(None, Some(factory_address)));
+        println!(" Liquidity: {:?}", pool.liquidity);
+
+        Ok(())
+
+    }
+}
+
