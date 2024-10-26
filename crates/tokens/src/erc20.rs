9ff2666c3700a4370609298053d544ee43f58f72
Peter Robinson <drinkcoffee@eml.cc>
2024-10-26 15:21:11 +1000

    Added address and total supply

--- crates/tokens/src/erc20.rs
+++ crates/tokens/src/erc20.rs
@@ -1,5 +1,5 @@
 use alloy::{
-    primitives::Address,
+    primitives::{Address, U256},
     providers::Provider,
     sol,
 };
@@ -53,8 +53,19 @@ impl<T, P> Erc20<T, P> where
         Ok(Self{token_contract})
     }
 
+    pub async fn address(&self) -> Result<&Address> {
+        let addr = self.token_contract.address();
+        Ok(addr)
+    }
+
     pub async fn symbol(&self) -> Result<String> {
         let tok0_symbol = self.token_contract.symbol().call().await?.sym;
         Ok(tok0_symbol)
     }
+
+    pub async fn total_supply(&self) -> Result<U256> {
+        let res = self.token_contract.totalSupply().call().await?.supply;
+        Ok(res)
+    }
+
 }
