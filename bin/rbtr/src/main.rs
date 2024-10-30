6b99201f32350228805706f92fd00436737192ea
James Snewin <james.snewin@immutable.com>
2024-10-30 12:32:41 +1000

    add deps and config toml

--- bin/rbtr/src/main.rs
+++ bin/rbtr/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    url_one: String,
+    url_two: String,
+    token_one: String,
+    token_two: String,
 }
+
+fn main() {}
