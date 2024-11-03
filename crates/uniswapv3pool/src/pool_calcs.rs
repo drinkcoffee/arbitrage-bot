710d9bf63176bc8b51a03ff56389b94e49960ebd
sergerad <serge.radinovich@immutable.com>
2024-11-04 10:32:55 +1300

    Handle error, underflow and denominator

--- crates/uniswapv3pool/src/pool_calcs.rs
+++ crates/uniswapv3pool/src/pool_calcs.rs
@@ -1,3 +1,4 @@
+use eyre::{eyre, Result};
 use uniswap_v3_sdk::prelude::FeeAmount;
 
 pub fn fee_to_float(fee: FeeAmount) -> f32 {
@@ -6,12 +7,24 @@ pub fn fee_to_float(fee: FeeAmount) -> f32 {
     fee_num / 10000.0
 }
 
-pub fn tick_to_exchange_rate(tick: i32, token_one_decimals: u64, token_two_decimals: u64) -> f64 {
+pub fn tick_to_exchange_rate(
+    tick: i32,
+    token_one_decimals: u64,
+    token_two_decimals: u64,
+) -> Result<f64> {
     let tick = tick as f64;
-    let val: f64 = 1.0001;
+    let base: f64 = 1.0001;
 
-    if token_one_decimals != token_two_decimals {
-        println!("Exchange when token decimals not the same not yet supported");
+    let token_decimals_diff = token_one_decimals.checked_sub(token_two_decimals);
+    match token_decimals_diff {
+        None => Err(eyre!(
+            "Token decimals subtraction overflow: ({} - {})",
+            token_one_decimals,
+            token_two_decimals
+        )),
+        Some(diff) => {
+            let denominator = 10u64.pow(diff.try_into()?);
+            Ok(base.powf(tick) / denominator as f64)
+        }
     }
-    val.powf(tick)
 }
